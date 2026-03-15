1ab1071d67cbacf4f7a69b1df1918baeb361a233
github-action-benchmark <github@users.noreply.github.com>
2026-03-15 11:19:49 +0000

    add Benchmark (pytest) benchmark result for c451610faf00e64caa28280f33a089af6d03181d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773536774403,
+  "lastUpdate": 1773573589493,
   "repoUrl": "https://github.com/sl4m3/ledgermind",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000096357763950357",
             "extra": "mean: 914.4292000030418 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "73834887+sl4m3@users.noreply.github.com",
+            "name": "Stanislav",
+            "username": "sl4m3"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c451610faf00e64caa28280f33a089af6d03181d",
+          "message": "🛡️ Sentinel: [HIGH] Fix command injection vulnerability (#80)\n\nFixed a potential command injection vulnerability in `src/ledgermind/server/background.py` where `subprocess.run` was using `shell=True`.\nReplaced it with a secure list-based argument passing and resolved the absolute path using `shutil.which`.",
+          "timestamp": "2026-03-15T06:17:31-05:00",
+          "tree_id": "c5356b7744a07aa5a27a0b78eeab9184d0710473",
+          "url": "https://github.com/sl4m3/ledgermind/commit/c451610faf00e64caa28280f33a089af6d03181d"
+        },
+        "date": 1773573588611,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/core/performance/bench_ops.py::test_benchmark_record_decision",
+            "value": 93.2764558379404,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00012041527484068217",
+            "extra": "mean: 10.720818999999437 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/core/performance/bench_ops.py::test_benchmark_search_fast_path",
+            "value": 11780.9774907417,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000010851567714709438",
+            "extra": "mean: 84.88260000376613 usec\nrounds: 5"
+          },
+          {
+            "name": "tests/core/performance/bench_ops.py::test_benchmark_search_hybrid_rrf",
+            "value": 1016.302301966231,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000125479672352568",
+            "extra": "mean: 983.9591999991626 usec\nrounds: 5"
           }
         ]
       }
