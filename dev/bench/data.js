da918e5f6489064db912201a341e2b6f399a332d
github-action-benchmark <github@users.noreply.github.com>
2026-03-15 01:06:14 +0000

    add Benchmark (pytest) benchmark result for 8dc79226a25440db61b87e8d5e967fdd3bb4b878

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772308471189,
+  "lastUpdate": 1773536774403,
   "repoUrl": "https://github.com/sl4m3/ledgermind",
   "entries": {
     "Benchmark": [
@@ -4712,6 +4712,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000007227262773424558",
             "extra": "mean: 51.0132000044905 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "staszotov555@gmail.com",
+            "name": "Stanislav Zotov",
+            "username": "sl4m3"
+          },
+          "committer": {
+            "email": "staszotov555@gmail.com",
+            "name": "Stanislav Zotov",
+            "username": "sl4m3"
+          },
+          "distinct": true,
+          "id": "8dc79226a25440db61b87e8d5e967fdd3bb4b878",
+          "message": "chore: untrack .jules and scripts folders",
+          "timestamp": "2026-03-15T03:50:31+03:00",
+          "tree_id": "2be553a4d6e44281c84f34c9cd8792491f96d79c",
+          "url": "https://github.com/sl4m3/ledgermind/commit/8dc79226a25440db61b87e8d5e967fdd3bb4b878"
+        },
+        "date": 1773536773872,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/core/performance/bench_ops.py::test_benchmark_record_decision",
+            "value": 123.4097694777429,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006428139772716505",
+            "extra": "mean: 8.103086199997733 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/core/performance/bench_ops.py::test_benchmark_search_fast_path",
+            "value": 12494.315087747427,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000017405047342065124",
+            "extra": "mean: 80.0363999928777 usec\nrounds: 5"
+          },
+          {
+            "name": "tests/core/performance/bench_ops.py::test_benchmark_search_hybrid_rrf",
+            "value": 1093.5783765398935,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000096357763950357",
+            "extra": "mean: 914.4292000030418 usec\nrounds: 5"
           }
         ]
       }
